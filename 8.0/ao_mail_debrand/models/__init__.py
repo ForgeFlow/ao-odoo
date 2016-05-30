1bebcf539a7646200345aae324825e78aca7bc39
jbeficent <jordi.ballester@eficent.com>
2016-05-30 13:22:39 +0200

    [IMP] Change module author

--- 8.0/ao_mail_debrand/models/__init__.py
+++ 8.0/ao_mail_debrand/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# © 2016 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import mail
