a5a81bb7cd141656a4815743d68c51cb4161c0c1
jbeficent <jordi.ballester@eficent.com>
2016-07-04 17:21:23 +0200

    [ADD] add module 'ao_stock_account'.
    Fixes TASK #3611
    
    Passes the refence to the sales order to the invoice, from the picking.

---
+++ 8.0/ao_stock_account/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "AO-specific customizations on stock account",
+    "version": "8.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "website": "http://www.eficent.com",
+    "category": "Stock",
+    "depends": ["stock_account"],
+    "license": "AGPL-3",
+    'installable': True,
+}
