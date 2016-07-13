fbfc2f222945f6ecfb3a7be728da48f03722bd38
jbeficent <jordi.ballester@eficent.com>
2016-07-13 14:36:49 +0200

    [IMP] various improvements

--- 8.0/account_due_list_days_overdue/__openerp__.py
+++ 8.0/account_due_list_days_overdue/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': "Payments Due list days overdue",
-    'version': '8.0.0.2.0',
+    'version': '8.0.0.1.0',
     'category': 'Accounting',
     'author': 'Odoo Community Association (OCA), '
               'Eficent Business and IT Consulting Services S.L., ',
@@ -14,6 +14,7 @@
     ],
     "data": [
         'views/account_move_line_view.xml',
+        'views/res_company_view.xml'
     ],
     "installable": True,
 }
