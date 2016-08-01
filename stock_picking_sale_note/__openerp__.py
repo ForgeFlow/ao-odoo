ffd1b96e5bad61a55d709ce439fda16d6e9322e7
jbeficent <jordi.ballester@eficent.com>
2016-08-01 18:22:10 +0200

    Set branch 8.0 with modules used in this version

---
+++ stock_picking_sale_note/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Picking Sale Note",
+    "version": "8.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "website": "http://www.eficent.com",
+    "category": "Sales",
+    "depends": ["sale_stock"],
+    "data": [
+        "views/stock_picking_view.xml"
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
