d347b7032c7ea154a7a389caf9e9a0c332174fc4
jbeficent <jordi.ballester@eficent.com>
2016-08-01 18:22:10 +0200

    Set branch 8.0 with modules used in this version

---
+++ ao_purchase_dept/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Purchase Dept",
+    "version": "8.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "website": "http://www.eficent.com",
+    "category": "Product",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_view.xml",
+        "views/account_invoice_view.xml"
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
