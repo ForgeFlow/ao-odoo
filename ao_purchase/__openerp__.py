b4a3a787a5f423e892c9be81934ef8dd00070191
jbeficent <jordi.ballester@eficent.com>
2016-08-10 15:29:35 +0200

    [IMP] migrated to 9.0 "ao_product" and "ao_purchase"

--- ao_purchase/__openerp__.py
+++ ao_purchase/__openerp__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "AO-specific customizations on purchase",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "Product",
