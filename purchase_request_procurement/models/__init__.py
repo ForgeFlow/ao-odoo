496253b40608650e80ddce09312be6e23d89f7a8
jbeficent <jordi.ballester@eficent.com>
2016-12-29 19:05:11 +0100

    [RM] remove purchase request modules. They are now in OCA

--- purchase_request_procurement/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
-# - Jordi Ballester Alomar
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-from . import purchase_request
-from . import procurement
-from . import product
