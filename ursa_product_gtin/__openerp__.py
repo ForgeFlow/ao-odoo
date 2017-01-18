b5ab7603f0abc1f441486cbf82a8d4ae99718d97
jbeficent <jordi.ballester@eficent.com>
2017-01-18 21:22:06 +0100

    [IMP] migrate 'ursa_product_gtin' to v9

--- ursa_product_gtin/__openerp__.py
+++ ursa_product_gtin/__openerp__.py
@@ -1,22 +1,34 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Ursa Information Systems
+#    Author: Balaji Kannan
+#    Copyright (C) 2013 (<http://www.ursainfosystems.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
 	"name" : "Product GTIN EAN8 EAN13 UPC JPC Support",
-	"version" : "1.2",
-	"author" : ["ChriCar Beteiligungs- und Beratungs- GmbH","Ursa Information Systems"],
-	"website" : ["http://www.chricar.at/ChriCar", "http://www.ursainfosystems.com"],
+	"version" : "9.0.1.2",
+	"author" : ["ChriCar Beteiligungs- und Beratungs- GmbH",
+				"Ursa Information Systems"],
+	"website" : ["http://www.chricar.at/ChriCar",
+				 "http://www.ursainfosystems.com"],
 	"category" : "Generic Modules/Others",
 	"depends" : ["product"],
-	"description" : """Replaces the EAN13 code completion with a checkroutine for EAN13, EAN8, JPC, UPC and GTIN
-    makes EAN visible in simplified view
-    YOU MUST comment constraints in product/product.py manually 
-    #_constraints = [(_check_ean_key, 'Error: Invalid ean code', ['ean13'])]
-    or apply the patch  provided in
-    https://bugs.launchpad.net/openobject-server/+bug/700451
-    Verified with OpenERP 7.0, added additional functions for each type of code to restrict use of code in fields
-    which will allow user to have multiple codes associated with the product.
-    By default view adds EAN13 and UPC fields to product form
-        """,
-	"data" : ["ursa_product_gtin_view.xml"],
-	"active": False,
-	"installable": False
+	"data" : ["views/ursa_product_gtin_view.xml"],
+	"installable": True
 }
--- ursa_product_gtin/models/ursa_product_gtin.py
+++ ursa_product_gtin/models/ursa_product_gtin.py
@@ -29,8 +29,10 @@
 import math
 from openerp import models, fields, api, _
 
+
 def is_pair(x):
-    return not x%2
+    return not x % 2
+
 
 def check_upc(upccode):
     if not upccode:
@@ -41,21 +43,23 @@ def check_upc(upccode):
         int(upccode)
     except:
         return False
-    sum=0
-    upc_len=len(upccode)
+    sum = 0
+    i = 0
+    check = 0
+    upc_len = len(upccode)
     for i in range(upc_len-1):
-        pos=int(upc_len-2-i)
+        pos = int(upc_len-2-i)
         if is_pair(i):
             sum += 3 * int(upccode[pos])
         else:
             sum += int(upccode[pos])
         check = int(math.ceil(sum / 10.0) * 10 - sum)
-
-	i += 1
-    if check != int(upccode[upc_len-1]): # last digit
+    i += 1
+    if check != int(upccode[upc_len-1]):# last digit
         return False
     return True
-    
+
+
 def check_ean(eancode):
     if not eancode:
         return True
@@ -65,7 +69,9 @@ def check_ean(eancode):
         int(eancode)
     except:
         return False
-    sum=0
+    sum = 0
+    i = 0
+    check = 0
     ean_len=len(eancode)
     for i in range(ean_len-1):
         pos=int(ean_len-2-i)
@@ -75,19 +81,21 @@ def check_ean(eancode):
             sum += int(eancode[pos])
         check = int(math.ceil(sum / 10.0) * 10 - sum)
 
-	i += 1
+    i += 1
     if check != int(eancode[ean_len-1]): # last digit
         return False
     return True
 
-class product_product(models.Model):
+
+class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    # this def shouldn't be necessary, but is not available from product_product
+    # this def shouldn't be necessary,
+    # but is not available from product_product
     @api.one
-    @api.constrains('ean13')
+    @api.constrains('barcode')
     def _check_ean_key(self):
-        res = check_ean(self.ean13)
+        res = check_ean(self.barcode)
         if not res:
             raise Warning(_('Invalid Bar Code Number'))
 
@@ -99,18 +107,29 @@ class product_product(models.Model):
         if not res:
             raise Warning(_('Invalid UPC Code Number'))
 
-    ean13 = fields.Char(string='EAN', help ='Barcode number for EAN8 EAN13 JPC GTIN http://en.wikipedia.org/wiki/Global_Trade_Item_Number')
-    upc = fields.Char(string='UPC', help='Barcode number for UPC http://en.wikipedia.org/wiki/Universal_Product_Code')
+    barcode = fields.Char(string='EAN',
+                          help='Barcode number for EAN8 EAN13 '
+                               'JPC GTIN http://en.wikipedia.org/'
+                               'wiki/Global_Trade_Item_Number')
+    upc = fields.Char(string='UPC',
+                      help='Barcode number for UPC '
+                           'http://en.wikipedia.org/wiki/'
+                           'Universal_Product_Code')
 
-    _sql_constraints = [('upc', 'UNIQUE(upc)', 'Cannot have duplicate UPC'), ('ean13', 'UNIQUE(ean13)','Cannot have duplicate EAN Code')]
+    _sql_constraints = [('upc', 'UNIQUE(upc)', 'Cannot have duplicate UPC'),
+                        ('barcode', 'UNIQUE(barcode)',
+                         'Cannot have duplicate EAN Code')]
+
 
 # ******* Just to be complete ****
 # the ean13 is defined in partner.py but apparently not used in any xml
-class res_partner(models.Model):
+class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    ean13 = fields.Char(string='EAN', help ='Barcode number for EAN8 EAN13 JPC GTIN')
-    upc = fields.Char(string='UPC', help ='Barcode number for UPC')
+    ean13 = fields.Char(string='EAN',
+                        help='Barcode number for EAN8 EAN13 JPC GTIN')
+    upc = fields.Char(string='UPC',
+                      help='Barcode number for UPC')
 
     @api.one
     @api.constrains('ean13')
@@ -127,7 +146,9 @@ class res_partner(models.Model):
         if not res:
             raise Warning(_('Invalid UPC Code Number'))
         
-    _sql_constraints = [('upc', 'UNIQUE(upc)', 'Cannot have duplicate UPC'), ('ean13', 'UNIQUE(ean13)','Cannot have duplicate EAN Code')]
+    _sql_constraints = [('upc', 'UNIQUE(upc)', 'Cannot have duplicate UPC'),
+                        ('ean13', 'UNIQUE(ean13)',
+                         'Cannot have duplicate EAN Code')]
 
 
 class ProductTemplate(models.Model):
