45e0be9f785966133a46544484ab863fe94540ad
Jordi Ballester <jordi.ballester@eficent.com>
2017-02-22 17:39:24 +0100

    [ADD] product_bom_standard_cost

---
+++ product_bom_standard_cost/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Product BOM Standard Cost",
+    "summary": "Shows the standard cost of a product that has a BOM "
+               "considering the BOM components and routing data",
+    "version": "9.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L., "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse",
+    "depends": ["mrp"],
+    "data": ["views/product_view.xml"],
+    "license": "AGPL-3",
+    'installable': True,
+}
