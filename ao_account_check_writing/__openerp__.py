c74adaf0841d26a63b9639de894b3f2c62f7d289
Jordi Ballester <jordi.ballester@eficent.com>
2017-04-13 22:59:27 +0200

    [IMP] - add module account_payment_show_invoices (task #8684) - add the check number to supplier payments (tasl #8684) - display the name of the partner of the warehouse instead of warehouse name in purchase order forms

--- ao_account_check_writing/__openerp__.py
+++ ao_account_check_writing/__openerp__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
-# - Jordi Ballester Alomar
+# © 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'AO Check Writing',
-    'version': '8.0.1.0.0',
-    'author': 'Eficent Business and IT Consulting Services S.L.',
+    'version': '9.0.1.0.0',
+    'author': 'Eficent',
     'category': 'Customizatins',
     'summary': 'Customizes AO check form',
     'website': 'http://www.openerp.com',
+    'depends': ['account_check_printing'],
+    'data': ['views/account_payment_view.xml'],
     'installable': True,
-    'active': False,
 }
