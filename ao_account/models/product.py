3f1ee6a1e602c47e5da298e492a71729fc508a65
lreficent <lois.rilo@eficent.com>
2017-04-21 13:32:50 +0200

    [9.0][FIX] Allow to change the 'Purchase Unit of Measure' in products.

---
+++ ao_account/models/product.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models
+from openerp.exceptions import UserError
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.multi
+    def write(self, vals):
+        try:
+            res = super(ProductTemplate, self).write(vals)
+        except UserError as e:
+            if e.name == 'You can not change the unit of ' \
+                         'measure of a product that has been already ' \
+                         'used in an account journal item. ' \
+                         'If you need to change the unit of measure, ' \
+                         'you may deactivate this product.':
+                return True
+            else:
+                raise
+        return res
