2751acdaad6a0794439ae7851d56dfbf82d178e2
Jordi Ballester <jordi.ballester@eficent.com>
2017-05-09 15:34:11 +0200

    add ao_crm_helpdesk_parse_email_lulzbot_support

---
+++ ao_crm_helpdesk_parse_email_lulzbot_support/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'AO CRM Helpdesk Parse Email Lulzbot Support',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'author': 'Eficent',
+    'website': 'http://www.eficent.com',
+    'category': 'CRM',
+    'depends': ['crm_helpdesk'],
+    'active': False,
+    'installable': True,
+}
---
+++ ao_crm_helpdesk_parse_email_lulzbot_support/models/crm_helpdesk.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openerp.osv import fields, osv, orm
+from openerp.tools import html2plaintext
+
+
+class CrmHelpdesk(osv.osv):
+
+    _inherit = "crm.helpdesk"
+
+    def _prepare_message_new_custom_values(self, cr, uid, msg,
+                                           custom_values=None, context=None):
+        custom_values, msg = super(
+            CrmHelpdesk, self)._prepare_message_new_custom_values(
+            cr, uid, msg, custom_values=custom_values, context=context
+        )
+
+        def parse_description(description):
+            fields = ['email', 'first and last name']
+            _dict = {}
+            description = description.lower()
+            for line in description.split('\n'):
+                for field in fields:
+                    if field in line:
+                        split_line = line.split(':')
+                        if len(split_line) > 1:
+                            _dict[field] = line.split(':')[1].strip()
+            return _dict
+        subject = msg.get('subject', '')
+        subject = subject.lower()
+        if 'support inquiry' in subject:
+            if custom_values is None:
+                custom_values = {}
+            desc = html2plaintext(msg.get('body')) if msg.get('body') else ''
+            _dict = parse_description(desc)
+            vals = {
+                'email_from': _dict.get('email'),
+                'contact_name': _dict.get('first and last name')
+            }
+            msg['from'] = _dict.get('email')
+            custom_values.update(vals)
+        return custom_values, msg
