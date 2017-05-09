d2efca90d0cf84c7123b2fe6acf531835a8baee4
Jordi Ballester <jordi.ballester@eficent.com>
2017-05-09 15:46:21 +0200

    add ao_mrp_bom_structure_xlsx

---
+++ ao_mrp_bom_structure_xlsx/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "AO - Export BoM Structure to Excel",
+    'version': '9.0.1.0.0',
+    'category': 'Manufacturing',
+    'author': "Eficent",
+    'license': 'AGPL-3',
+    "depends": ['report_xlsx', 'mrp', 'product_manufacturer'],
+    "data": [
+        'report/bom_structure_xlsx.xml',
+    ],
+    "installable": True
+}
