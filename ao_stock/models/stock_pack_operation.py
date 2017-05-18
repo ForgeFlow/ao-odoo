f49d7a2c1ba157a21760249d7d60123d89a7f43d
lreficent <lois.rilo@eficent.com>
2017-05-18 17:41:05 +0200

    manage uom and display it.

--- ao_stock/models/stock_pack_operation.py
+++ ao_stock/models/stock_pack_operation.py
@@ -12,9 +12,11 @@ class StockPicking(models.Model):
     def _compute_qty_available_in_source_loc(self):
         product_available = self.product_id.with_context(
             location=self.location_id.id)._product_available()[
-            self.product_id.id]
-        self.qty_available_in_source_loc = product_available[
-            'qty_available_not_res']
+            self.product_id.id]['qty_available_not_res']
+        res = self.product_uom_id._compute_qty(
+            self.product_id.product_tmpl_id.uom_id.id, product_available,
+            self.product_uom_id.id)
+        self.qty_available_in_source_loc = res
 
     qty_available_in_source_loc = fields.Float(
         compute=_compute_qty_available_in_source_loc)
