f1755d1668b63ebeb51f2c4caafa5412a4339a8f
lreficent <lois.rilo@eficent.com>
2017-06-14 11:58:08 +0200

    [9.0][IMP] ao_crm_helpdesk: add problem tracking to helpdesk.

--- ao_crm_helpdesk/__openerp__.py
+++ ao_crm_helpdesk/__openerp__.py
@@ -4,11 +4,15 @@
 
 {
     "name": "AO-specific customizations on crm helpdesk",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "CRM",
-    "depends": ["crm_helpdesk"],
+    "depends": ["crm_helpdesk", "quality_control_issue"],
     "license": "AGPL-3",
-    'installable': True,
+    "data": [
+        "views/crm_helpdesk_view.xml",
+        "views/qc_problem_track_view.xml",
+    ],
+    "installable": True,
 }
