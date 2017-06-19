c364b58b6ce9c3733bd89d82b4ef9ae7b5c1392a
lreficent <lois.rilo@eficent.com>
2017-06-19 13:51:21 +0200

    [9.0][MOV] Move code to add problem tracking helpdesk tickets to a separate module.

--- ao_crm_helpdesk/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-from . import crm_helpdesk
-from . import qc_problem_track
