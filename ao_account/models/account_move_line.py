66ae3062f241e0c17c9b1a97bda56a42548d66f4
lreficent <lois.rilo@eficent.com>
2017-08-17 16:41:39 +0200

    [ADD] small hook for t_12963 script.

---
+++ ao_account/models/account_move_line.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.multi
+    def update_amount_residual(self):
+        """Hook method used in the script t_12963."""
+        super(AccountMoveLine, self)._amount_residual()
+        return True
