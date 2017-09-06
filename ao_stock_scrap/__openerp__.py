b1d73100997c43286d5db9bab9bd19fe136a6b8a
lreficent <lois.rilo@eficent.com>
2017-09-06 17:51:58 +0200

    [9.0][ADD] ao_stock_scrap

---
+++ ao_stock_scrap/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "AO-specific customizations on stock scrap",
+    "version": "9.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "website": "http://www.eficent.com",
+    "category": "Warehouse Management",
+    "depends": ["stock_scrap"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/stock_scrap_security.xml",
+        "views/stock_scrap_view.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+    # 'auto_install': True,
+}
---
+++ ao_stock_scrap/models/stock_scrap.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+
+
+class StockScrap(models.Model):
+    _inherit = "stock.scrap"
+
+    company_id = fields.Many2one(
+        comodel_name='res.company', string='Company', required=True,
+        default=lambda self: self.env.user.company_id)
+
+    scrap_location_id = fields.Many2one(required=True)
