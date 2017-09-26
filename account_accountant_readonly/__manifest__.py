21783272a7caf22f45e408fb8acc7fa98bdc2c0f
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-09-26 19:12:40 +0530

    [MIG] account_accountant_readonly v10

---
+++ account_accountant_readonly/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Add Read-Only Accountant Group',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'summary': """Adds a read-only accountant group to have an external
+                  accountant go through your accounting records in Odoo.""",
+    'author': 'Rooms For (Hong Kong) Limited T/A OSCG',
+    'website': 'http://www.openerp-asia.net/',
+    'depends': ['account_accountant'],
+    'data': [
+        'security/account_security.xml',
+        'security/ir.model.access.csv',
+        ],
+    'installable': True,
+}
