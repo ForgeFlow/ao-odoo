ae0e3343162b30ccfa055283a918199e64efd5aa
lreficent <lois.rilo@eficent.com>
2017-10-09 13:07:59 +0200

    [9.0][ADD] ao_stock_picking_operation_quick_change

---
+++ ao_stock_picking_operation_quick_change/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Lois Rilo <lois.rilo@eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "AO customization for: Stock Picking Operation Quick Change",
+    "summary": "Customization for Aleph Objects.",
+    "version": "9.0.1.0.0",
+    "category": "Inventory",
+    "website": "http://www.eficent.com",
+    "author": "Eficent",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "stock_picking_operation_quick_change",
+    ],
+    "data": [
+        "wizards/stock_picking_wizard_view.xml",
+    ],
+}
---
+++ ao_stock_picking_operation_quick_change/wizards/stock_picking_wizard.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import _, api, fields, models
+from openerp.exceptions import UserError
+
+
+class StockPickingOperationWizard(models.TransientModel):
+    _inherit = 'stock.picking.operation.wizard'
+
+    change_all = fields.Boolean(default=True)
+
+    @api.multi
+    def action_apply(self):
+        res = super(StockPickingOperationWizard, self).action_apply()
+        if self.change_all:
+            vals = {'location_dest_id': self.new_location_dest_id.id}
+            pickings = self.env['stock.picking'].browse(
+                self.env.context['active_ids'])
+            pickings.write(vals)
+            for picking in pickings:
+                picking.message_post(body=_(
+                    "Destination location changed to %s"
+                    % self.new_location_dest_id.name))
+        return res
