c628eadde8e986b1f73ac868139b267a916bc8c3
aheficent <ahenriquez@eficent.com>
2017-10-19 18:13:39 +0200

    [MIG]product_bom_standard_cost to v10

--- product_bom_standard_cost/models/product.py
+++ product_bom_standard_cost/models/product.py
@@ -19,34 +19,20 @@ class ProductTemplate(models.Model):
             template.bom_standard_cost = 0.0
             price = 0.0
             for variant in template.product_variant_ids:
-                bom_id = bom_obj._bom_find(product_id=variant.id)
-                bom = bom_obj.sudo().browse(bom_id)
+                bom = bom_obj._bom_find(product=variant)
                 for sbom in bom.bom_line_ids:
-                    my_qty = sbom.product_qty / sbom.product_efficiency
+                    my_qty = sbom.product_qty
                     if not sbom.attribute_value_ids:
                         # No attribute_value_ids means the bom line
                         # is not variant specific
-                        price += uom_obj._compute_price(
-                            sbom.product_id.uom_id.id,
+                        price += sbom.product_id.uom_id._compute_price(
                             sbom.product_id.bom_standard_cost,
-                            sbom.product_uom.id) * my_qty
-
-                if bom.routing_id:
-                    for wline in bom.routing_id.workcenter_lines:
-                        wc = wline.workcenter_id
-                        cycle = wline.cycle_nbr
-                        hour = (wc.time_start + wc.time_stop + cycle *
-                                wc.time_cycle) * (wc.time_efficiency or 1.0)
-                        price += wc.costs_cycle * cycle + wc.costs_hour * hour
-                        price = uom_obj._compute_price(
-                            bom.product_uom.id, price,
-                            bom.product_id.uom_id.id)
-
+                            sbom.product_uom_id) * my_qty
+                # Todo: cost when using workcenters
                 # Convert on product UoM quantities
                 if price > 0:
-                    price = uom_obj._compute_price(bom.product_uom.id,
-                                                   price / bom.product_qty,
-                                                   bom.product_id.uom_id.id)
+                    price = bom.product_uom_id._compute_price(
+                        price / bom.product_qty, bom.product_id.uom_id)
                 else:
                     price = template.standard_price
             template.bom_standard_cost = price
