395fc7e946a65b948f887fce351629c04247d99f
lreficent <lois.rilo@eficent.com>
2017-11-24 18:33:23 -0500

    [9.0][ADD] ao_web

---
+++ ao_web/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "AO-specific customizations on Web",
+    "summary": "AO Customization for Web module.",
+    "category": "Website",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/ao_assets_backend.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
