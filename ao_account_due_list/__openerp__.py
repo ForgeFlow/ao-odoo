9da774749b094f7f8ced66700c481740e52e996c
lreficent <lois.rilo@eficent.com>
2017-12-29 11:53:11 +0100

    [9.0][FIX] ao_account_due_list: bump version number and minor fixes

--- ao_account_due_list/__openerp__.py
+++ ao_account_due_list/__openerp__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "AO-specific customizations on account_due_list",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "Accounting ",
