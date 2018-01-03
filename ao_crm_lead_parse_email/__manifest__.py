f3037e91107d843ac7f39bc7c10155403ffa124e
lreficent <lois.rilo@eficent.com>
2018-01-03 18:31:55 +0100

    [10.0][MIG] ao_crm_lead_parse_email

--- ao_crm_lead_parse_email/__manifest__.py
+++ ao_crm_lead_parse_email/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': 'AO CRM Lead Parse Email',
-    'version': '1.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Eficent',
     'website': 'http://www.eficent.com',
@@ -12,6 +12,5 @@
     'data': [
         "data/crm_lead_parse_email_config_data.xml",
     ],
-    'active': False,
     'installable': True,
 }
