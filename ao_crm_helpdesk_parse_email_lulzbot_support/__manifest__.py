d396936102823154e7049bcf831aaff88a7bbce1
lreficent <lois.rilo@eficent.com>
2018-01-03 18:17:29 +0100

    [10.0][MIG] ao_crm_helpdesk_parse_email_lulzbot_support

--- ao_crm_helpdesk_parse_email_lulzbot_support/__manifest__.py
+++ ao_crm_helpdesk_parse_email_lulzbot_support/__manifest__.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': 'AO CRM Helpdesk Parse Email Lulzbot Support',
-    'version': '1.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Eficent',
     'website': 'http://www.eficent.com',
     'category': 'CRM',
     'depends': ['crm_helpdesk'],
-    'active': False,
     'installable': True,
     'data': [
         "data/crm_helpdesk_parse_email_config_data.xml",
