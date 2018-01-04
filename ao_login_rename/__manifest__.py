ef5d98bed4ecbc657778b925929c584d1334e373
lreficent <lois.rilo@eficent.com>
2018-01-04 11:21:41 +0100

    [10.0]MIG] ao_login_rename

--- ao_login_rename/__manifest__.py
+++ ao_login_rename/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016-18 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "AO Login Rename",
-    "summary": "Renames the login from Email Address to Login and Username",
-    "version": "9.0.1.0.0",
+    "summary": "Renames the login from Email Address to Username.",
+    "version": "10.0.1.0.0",
     "category": "Customizations",
     "website": "http://www.eficent.com",
     "author": "Eficent Business and IT Consulting Services S.L.",
@@ -16,6 +16,5 @@
     ],
     "data": [
         'views/template.xml',
-        'views/res_users_view.xml'
     ],
 }
