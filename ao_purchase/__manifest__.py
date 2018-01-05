4115a70eb3ca5a2dd2c4f212bbc7d3551d692a7a
lreficent <lois.rilo@eficent.com>
2018-01-05 13:06:52 +0100

    [10.0][MIG] ao_purchase

--- ao_purchase/__manifest__.py
+++ ao_purchase/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
+# Copyright 2015-18 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "AO-specific customizations on purchase",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "Product",
@@ -13,7 +13,7 @@
     "data": [
         "views/purchase_order_view.xml",
         "views/product_view.xml",
-        "views/report_purchase_order.xml"
+        "views/report_purchase_order.xml",
     ],
     "license": "AGPL-3",
     'installable': True,
