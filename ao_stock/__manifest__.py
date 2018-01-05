802660126d256eb67bb6877a2525c236b4968639
lreficent <lois.rilo@eficent.com>
2018-01-05 16:05:35 +0100

    [10.0][MIG] ao_stock

--- ao_stock/__manifest__.py
+++ ao_stock/__manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
+# Copyright 2015-18 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "AO-specific customizations on stock",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "Warehouse Management",
     "depends": ["stock", "stock_inventory_exclude_sublocation"],
     "data": [
-        # "security/ao_stock_security.xml",
+        # "security/ao_stock_security.xml", --not approved yet
         # "security/ir.model.access.csv",
         "views/stock_view.xml",
         "views/report_stockpicking_operations.xml",
