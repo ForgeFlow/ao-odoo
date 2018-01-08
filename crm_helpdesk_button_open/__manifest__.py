c6d0b26bd5493fe63c2bcee9afef75fb6ae5733f
lreficent <lois.rilo@eficent.com>
2018-01-08 09:20:44 +0100

    [10.0][MIG] crm_helpdesk_button_open

--- crm_helpdesk_button_open/__manifest__.py
+++ crm_helpdesk_button_open/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "CRM Helpdesk Button Open",
     "summary": "Button to open a draft Helpdesk ticket and assign the current "
                "user as owner.",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.,"
               "Odoo Community Association (OCA)",
     "website": "https://www.odoo-community.org",
