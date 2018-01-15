1f7de6c99593a71c90e1192c2bec502bdcfc70f8
lreficent <lois.rilo@eficent.com>
2018-01-15 17:04:20 +0100

    [10.0][UPD] ao_profile

--- ao_profile/__manifest__.py
+++ ao_profile/__manifest__.py
@@ -1,126 +1,132 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
+# Copyright 2015-18 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name' : 'AO Profile',
-    'version': '9.0.1.0.0',
+    'name': 'AO Profile',
+    'version': '10.0.1.0.0',
     'author': 'Aleph Objects Inc.',
     'summary': 'Contains the modules used by AO',
     'website': 'http://www.eficent.com',
     'category': 'Customizations',
     'depends': [
         # Odoo standard
-        'account_asset',
+        # -------------
+        # 'account_analytic_analysis', -- deprecated
+        # 'account_analytic_default', -- deprecated
+        # 'account_analytic_plans', -- deprecated
+        # 'account_anglo_saxon', -- deprecated
+        # 'account_check_writing', --deprecated
+        # 'account_followup', -- deprecated
+        # 'account_payment', -- deprecated
         # 'analytic_contract_hr_expense', -- deprecated
-        'crm',
-        'mail',
-        'account_voucher',
-        'point_of_sale',
-        'project',
-        'note',
-        'project_issue',
-        'account_accountant',
-        'sale',
-        'stock',
-        'stock_account',
-        'mrp',
-        'purchase',
-        'hr',
-        'hr_timesheet',
-        'hr_timesheet_sheet',
-        'hr_holidays',
-        'hr_expense',
-        'hr_payroll',
+        # 'analytic_user_function', -- deprecated
+        # 'base_report_designer', -- deprecated
+        # 'claim_from_delivery', -- deprecated in v10
+        # 'crm_profiling', --- deprecated
+        # 'edi', -- deprecated
+        # 'email_template', -- deprecated
+        # 'hr_timesheet_invoice', -- deprecated
+        # 'marketing', --deprecated in v10
+        # 'marketing_crm', -- deprecated
+        # 'mrp_operations', --deprecated in v10
+        # 'procurement_jit',  -- not with ddmrp (beware: it's auto-installed)
+        # 'procurement_jit_stock', -- deprecated
+        # 'project_timesheet',  # Merged in hr_timesheet in v10
+        # 'purchase_double_validation', -- deprecated
+        # 'report_webkit', -- deprecated in v10 (openupgrade pending)
+        # 'sale_journal', -- deprecated
+        # 'share', -- deprecated
+        # 'warning', --deprecated in v10
+        # 'web_gantt', -- deprecated
+        # 'web_graph', -- deprecated
+        # 'web_kanban_sparkline', -- deprecated
+        # 'web_tests', -- deprecated
+        # 'web_view_editor', --deprecated in v10
         'account',
-        #'account_analytic_default', -- deprecated
-        #'account_analytic_plans', -- deprecated
+        'account_accountant',
+        'account_asset',
         'account_budget',
         'account_cancel',
-        # 'account_followup', -- deprecated
-        #'account_payment', -- deprecated
+        'account_check_printing',
+        'account_voucher',
         'analytic',
-        #'analytic_user_function', -- deprecated
-        'auth_signup',
         'auth_crypt',
+        'auth_signup',
         'base',
         'base_action_rule',
         'base_iban',
         'base_import',
-        #'base_report_designer', -- deprecated
         'base_setup',
         'board',
         'bus',
         'calendar',
-        'claim_from_delivery',
-        'crm_claim',
-        'crm_helpdesk',
-        #'crm_profiling', --- deprecated
+        'crm',
         'decimal_precision',
         'delivery',
         'document',
-        #'edi', -- deprecated
-        #'email_template', -- deprecated
         'fetchmail',
+        'hr',
         'hr_attendance',
         'hr_contract',
+        'hr_expense',
+        'hr_holidays',
+        'hr_payroll',
         'hr_payroll_account',
-        #'hr_timesheet_invoice', -- deprecated
+        'hr_timesheet',
+        'hr_timesheet_sheet',
         'lunch',
-        'marketing',
+        'mail',
         'marketing_campaign',
-        #'marketing_crm', -- deprecated
+        'mrp',
         'mrp_byproduct',
-        'mrp_operations',
         'mrp_repair',
+        'note',
         'payment',
         'payment_transfer',
+        'point_of_sale',
         'procurement',
-        # 'procurement_jit',  -- not with ddmrp (beware: it's auto-installed)
-        #'procurement_jit_stock', -- deprecated
         'product',
-        'project_issue_sheet',
-        'project_timesheet',
-        #'purchase_double_validation', -- deprecated
+        'project',
+        'project_issue',  # gives error
+        'project_issue_sheet',  # gives error
+        'purchase',
         'purchase_requisition',
         'report',
         'resource',
+        'sale',
         'sale_crm',
-        #'sale_journal', -- deprecated
         'sale_margin',
         'sale_mrp',
         'sale_order_dates',
-        'sales_team',
         'sale_stock',
-        #'share', -- deprecated
+        'sales_team',
+        'stock',
+        'stock_account',
         'subscription',
-        'warning',
-        #'account_check_writing', --deprecated
-        'account_check_printing',
-        'web_calendar',
-        #'account_anglo_saxon', -- deprecated
         'web',
+        'web_calendar',
         'web_diagram',
-        #'web_gantt', -- deprecated
-        #'web_graph', -- deprecated
         'web_kanban',
         'web_kanban_gauge',
-        #'web_kanban_sparkline', -- deprecated
-        # 'web_tests', -- deprecated
-        'web_view_editor',
-        'report_webkit',
-        #'account_analytic_analysis', -- deprecated
         #
         # OCA/account-financial-reporting
+        # -------------------------------
         #'account_financial_report_webkit', -- deprecated
         'account_financial_report_qweb',
-        'mis_builder',
-        'customer_outstanding_statement',
         'customer_activity_statement',
+        'customer_outstanding_statement',
+        'account_tax_balance',
+        #
+        # OCA/mis-builder
+        # ---------------
+        'mis_builder',
         #
         # OCA/account-financial-tools
+        # ---------------------------
         'account_move_line_purchase_info',
+        #
         # OCA/product-attribute
         # ---------------------
         'product_manufacturer',
@@ -132,56 +138,74 @@
         #
         # OCA/server-tools
         # ----------------
-        'base_technical_features',
-        'database_cleanup',
         'auditlog',
         'base_search_fuzzy',
+        'base_technical_features',
+        'database_cleanup',
         'disable_odoo_online',
         #
         # OCA/stock-logistics-warehouse
         # ---------------------
-        'stock_putaway_product',
-        'stock_orderpoint_manual_procurement',
-        'stock_orderpoint_uom',
-        'stock_inventory_revaluation',
-        'stock_valuation_account_manual_adjustment',
         'account_move_line_product',
-        'stock_quant_reserved_qty_uom',
         'account_move_line_stock_info',
-        'stock_quant_merge',
         'stock_account_quant_merge',
+        'stock_available_unreserved',
         'stock_cycle_count',
-        'stock_inventory_verification_request',
+        'stock_demand_estimate',
         'stock_inventory_exclude_sublocation',
+        'stock_inventory_revaluation',
+        'stock_inventory_verification_request',
+        'stock_orderpoint_manual_procurement',  # under rework
+        'stock_orderpoint_uom',
+        'stock_putaway_product',
+        'stock_quant_merge',
+        'stock_quant_reserved_qty_uom',
+        'stock_valuation_account_manual_adjustment',  # to be migrated
+        'stock_warehouse_orderpoint_stock_info',
+        'stock_warehouse_orderpoint_stock_info_unreserved',
+        'stock_inventory_chatter',
+        'stock_removal_location_by_priority',
+        'procurement_auto_create_group',
+        'stock_account_change_product_valuation',
         # 'stock_picking_back2draft', Deprecated - See
         # https://github.com/OCA/stock-logistics-workflow/pull/276
         #
+        # OCA/stock-logistics-workflow
+        # ----------------------------
+        'stock_disable_force_availability_button',
+        'stock_no_negative',
+        'stock_picking_operation_quick_change',
+        #
         # OCA/account-invoicing
         # ---------------------
+        # 'account_invoice_refund_option', -- Deprecaded, https://github.com/OCA/account-invoicing/pull/229
         'account_invoice_merge',
-        'account_invoice_refund_option',
+        'account_invoice_view_payment',
         'purchase_stock_picking_return_invoicing',
         'sale_stock_picking_return_invoicing',
-        'account_invoice_view_payment',
+        'purchase_stock_picking_return_invoicing_open_qty',  # TODO: not migrated
+        'account_invoice_fixed_discount',
         #
         # OCA/purchase-workflow
         # ---------------------
         'product_by_supplier',
+        'purchase_delivery_split_date',
+        'purchase_delivery_split_date',
         'purchase_location_by_line',
-        'purchase_delivery_split_date',
-        'purchase_request_procurement',
+        'purchase_open_qty',
+        'purchase_order_approved',
         'purchase_request',
-        'purchase_request_to_requisition',
-        'purchase_request_to_rfq',
-        'purchase_delivery_split_date',
         'purchase_request_department',
+        'purchase_request_procurement',
+        'purchase_request_to_procurement',
+        'purchase_request_to_requisition',  # Maybe not needed
+        'purchase_request_to_rfq',
+        'purchase_request_to_rfq_order_approved',
         'subcontracted_service',
-        'purchase_order_approved', # PR:https://github.com/OCA/purchase-workflow/pull/381
-        'purchase_request_to_rfq_order_approved', # PR:https://github.com/OCA/purchase-workflow/pull/388
         #
         # OCA/hr
         # ------
-        'hr_holiday_notify_employee_manager',
+        'hr_holidays_notify_employee_manager',  # v9: hr_holiday_notify_employee_manager
         #
         # OCA/hr-timesheet
         # ----------------
@@ -193,37 +217,42 @@
         'web_sheet_full_width',
         'web_export_view',
         'web_tree_dynamic_colored_field',
+        'web_tree_many2one_clickable',
         #
         # OCA/sale-workflow
         # -----------------
-        # 'sale_stock_picking_back2draft', -- not yet ported to v9
-        'sale_procurement_group_by_line',
         # 'sale_sourced_by_line',  -- not needed, feature is out-of-the-box.
+        # 'sale_stock_picking_back2draft', -- not yet ported to v9, see stock_picking_back2draft
         'sale_force_invoiced',
+        'sale_procurement_group_by_line',
+        'sale_product_set',
         'sale_validity',
+        'sale_fixed_discount',
         #
-        # OCA/bank-statement-reconcile
-        # 'account_easy_reconcile', -- migrated to account_mass_reconcile
-        # 'account_advanced_reconcile', -- migrated to account_mass_reconcile
-        # 'account_mass_reconcile', -- pending to install
-        # 'account_advanced_reconcile_transaction_by_purchase_line'. pending to migrate
-        #
-        # OCA/connector-telephony
-        # 'asterisk_click2dial', -- not used
-        # 'base_phone', -- not used
-        # 'crm_phone', -- not used
-        # 'asterisk_click2dial_crm', -- deprecated
+        # OCA/account-reconcile
+        # ---------------------
+        'account_mass_reconcile',
+        'account_mass_reconcile_by_purchase_line',
         #
         # OCA/crm
+        # -------
+        'crm_claim',
+        'crm_helpdesk',
         'crm_deduplicate_acl',
         'crm_sale_marketing',
         #
-        # OCA/social
-        # 'mail_debrand', -- Pending to migrate
-        # 'mail_message_name_search', --deprecated
-        'mail_auto_follower_notify',
+        # OCA/partner-contact
+        # -------------------
+        'partner_contact_job_position',
+        'partner_sector',
         #
+        # OCA/social
+        # ----------
+        # 'mail_debrand', -- integrated in ao_mail
+        # 'mail_message_name_search', --deprecated
         'base_search_mail_content',
+        'mail_tracking',
+        #
         # OCA/account-payment
         # -------------------
         'account_due_list',
@@ -232,11 +261,17 @@
         'account_check_printing_report_base',
         'account_check_printing_report_dlt103',
         'account_partner_reconcile',
-        'account_payment_show_invoice', #PR: https://github.com/OCA/account-payment/pull/133
+        'account_payment_show_invoice',
         #
         # OCA/manufacture
         # -------------------------
+        # 'mrp_production_unreserve', -- not needed in v10
         'mrp_bom_component_menu',
+        'mrp_mto_with_stock',
+        'mrp_production_putaway_strategy',
+        'mrp_production_request',  #TODO: To be migrated to v10
+        # 'mrp_disable_force_availability_button', -- deprecated, trust stock_no_negative
+        #
         # OCA/manufacture-reporting
         # -------------------------
         'mrp_bom_structure_report_level_1',
@@ -247,77 +282,84 @@
         # ---------------------
         'project_task_report',
         #
-        # Vauxoo
-        # 'product_do_merge', -- deprecated
-        #
-        # WilldooIT/Pentaho-reports-for-OpenERP
-        # 'pentaho_reports' -- deprecated
-        #
         # Eficent/ddmrp
         # -------------
-        'ddmrp',
-        'stock_available_unreserved',
-        'stock_demand_estimate',
-        'stock_warehouse_orderpoint_stock_info',
+        'ddmrp', # to be migrated to v10
+        'ddmrp_exclude_moves_adu_calc',
+        'ddmrp_history',
+        'ddmrp_mrp_production_request'
+        'ddmrp_product_replace',
+        'procurement_service',  # also OCA/manufacture, really needed?
+        #
         # 'account_bank_statement_extensions', -- deprecated
         # 'l10n_be_coda', -- deprecated
         # 'l10n_be_invoice_bba', -- deprecated
-        # 'account_financial_report_webkit_xls', --deprecated
-        # 'report_xls', -- deprecated
-        # OCA/reporting-engine
         #
+        # OCA/reporting-engine
+        # --------------------
+        'bi_sql_editor',
         'bi_view_editor',
+        'report_wkhtmltopdf_param',
         #
         # ao-odoo
         # -------
-        'smile_decimal_precision',
-        # 'ursa_web_style', -- deprecated
-        # 'partner_aging', -- deprecated
-        'ursa_product_reference',
-        'usa_localization',
-        'ursa_product_gtin',
         # 'account_fstr', -- deprecated
-        'ursa_crmlead',
-        'ursa_helpdesk',
-        'ursa_product_customizations',
-        'ursa_ref_partner',
-        # 'stock_picking_preview_putaway_location', -- deprecated
-        # 'ursa_sale_customizations', -- deprecated
-        # 'ursa_shipwire', -- deprecated
-        'ao_account',
         # 'ao_account_check_writing', -- deprecated
-        'ao_account_due_list',
         # 'ao_account_voucher', -- deprecated
         # 'ao_crm_helpdesk', -- deprecated
         # 'ao_hr', -- deprecated
+        # 'ao_hr_holidays',  -- standard behavior since v10
         # 'ao_hr_payroll', -- deprecated
-        'ao_hr_timesheet_sheet',
-        'ao_hr_holidays',
-        'ao_login_rename',
         # 'ao_mail_debrand', --pending to migrate
+        # 'ao_product', -- standard behavior since v10
+        # 'ao_project', -- deprecated in v10
+        # 'ao_stock_account', --deprecated
+        # 'mail_auto_follower_notify', -- seems to work out of the box. needed?
+        # 'partner_aging', -- deprecated
+        # 'product_uom_change_fix', --deprecated
+        # 'smile_decimal_precision', -- deprecated, not needed.
+        # 'stock_picking_preview_putaway_location', -- deprecated
+        # 'stock_picking_report_reserved_quant', -- deprecated
+        # 'ursa_purchase_customizations', --deprecated
+        # 'ursa_sale_customizations', -- deprecated
+        # 'ursa_shipwire', -- deprecated
+        # 'ursa_web_style', -- deprecated
+        'account_anglo_saxon_no_cogs_deferral',
+        'ao_account',
+        'ao_account_due_list',
+        'ao_base',
+        'ao_crm',
+        'ao_crm_helpdesk_parse_email_lulzbot_support',
+        'ao_crm_lead_parse_email',
+        'ao_hr_timesheet_sheet',
+        'ao_login_rename',
+        'ao_mail',
         'ao_mrp',
-        'ao_product',
-        'ao_project',
         'ao_purchase',
         'ao_purchase_dept',
         'ao_sale',
-        'ao_crm',
-        # 'ao_stock', -- deprecated
-        # 'ao_stock_account', --deprecated
-        'stock_picking_menu',
-        'stock_picking_sale_note',
-        'account_anglo_saxon_no_cogs_deferral',
+        'ao_stock',
+        'ao_stock_picking_operation_quick_change',
+        'ao_stock_scrap',
+        'ao_utm',
+        'ao_web',
+        'crm_helpdesk_add_suggested_recipient',  # needed?
+        'crm_helpdesk_button_open',
+        'crm_helpdesk_warning',
         'mrp_production_calculate_cost_finished_product',
+        'mrp_production_service',  # issue: procurement are not being set to done.
         'mrp_production_update_product_price',
         'product_bom_standard_cost',
-        'stock_account_change_product_valuation',
-        'crm_helpdesk_button_open',
-        'crm_helpdesk_add_suggested_recipient',
-        'mrp_production_service',
         'project_task_work',
-        # 'product_uom_change_fix', --deprecated
-        # 'stock_picking_report_reserved_quant', -- deprecated
-        # 'ursa_purchase_customizations', --deprecated
+        'stock_picking_menu',
+        'stock_picking_sale_note',
+        'ursa_crmlead',
+        'ursa_helpdesk',
+        'ursa_product_customizations',
+        'ursa_product_gtin',
+        'ursa_product_reference',
+        'ursa_ref_partner',
+        'usa_localization',
     ],
     'installable': True,
 }
