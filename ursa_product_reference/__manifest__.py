ee49ee840d9283930f6c31de3005fe6c6955112b
lreficent <lois.rilo@eficent.com>
2018-01-15 16:27:58 +0100

    [10.0][MIG] ursa_product_reference

---
+++ ursa_product_reference/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013 Ursa Information Systems (http://www.ursainfosystems.com).
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Product Internal Reference Generation",
+    "version": "10.0.1.0.0",
+    "author": "Ursa Information Systems, USA",
+    "license": "AGPL-3",
+    "category": "Stock",
+    "summary": "Generate product internal reference number",
+    "website": "http://www.ursainfosystems.com",
+    "depends": ["base", "stock", "product"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/productclass_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
