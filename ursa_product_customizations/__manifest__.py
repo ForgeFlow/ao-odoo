39d1d0bad569230430b7ba2660aba43cff89b1d4
lreficent <lois.rilo@eficent.com>
2018-01-15 15:40:51 +0100

    [10.0][MIG] ursa_product_customizations

---
+++ ursa_product_customizations/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013 Ursa Information Systems (http://www.ursainfosystems.com).
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Product Customizations",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Ursa Information Systems, USA",
+    "category": "Product",
+    "summary": "Customization on Product",
+    "website": "http://www.ursainfosystems.com",
+    "depends": ["base", "product", "decimal_precision", "mail"],
+    "data": [
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}
