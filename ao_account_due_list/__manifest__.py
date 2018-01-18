b9543e899118578d1b0658ff00b5517f5ffaa56e
Lois Rilo <lois.rilo@eficent.com>
2018-01-18 12:57:24 +0100

    [10.0][MIG] ao_account_due_list

--- ao_account_due_list/__manifest__.py
+++ ao_account_due_list/__manifest__.py
@@ -5,13 +5,13 @@
 
 {
     "name": "AO-specific customizations on account_due_list",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "Accounting ",
     "depends": ["account_due_list_days_overdue"],
     "data": [
-        "views/account_move_line_view.xml"
+        "views/account_move_line_view.xml",
     ],
     "license": "AGPL-3",
     'installable': True,
