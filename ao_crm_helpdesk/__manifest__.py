7c41aeffafd39bd96a76d71e188b93eb5380b8dd
Lois Rilo <lois.rilo@eficent.com>
2018-03-27 12:16:59 +0200

    [10.0][MIG] ao_crm_helpdesk

--- ao_crm_helpdesk/__manifest__.py
+++ ao_crm_helpdesk/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "AO-specific customizations on crm helpdesk",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "CRM",
