8c791e32a77d2abbf90be7fd97e589a9a9174d37
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-05-23 12:42:25 +0200

    add account_analytic_default_purchase

---
+++ account_analytic_default_purchase/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Analytic Default",
+    "summary": "Set default values for analytic accounts in purchases",
+    "version": "9.0.1.0.0",
+    "category": "Accounting",
+    "author": "Eficent",
+    "license": "AGPL-3",
+    "depends": [
+        "account_analytic_default",
+        "purchase"
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": True,
+}
