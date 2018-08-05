bfd0351868aee219e65028efd43230f22d85f2c3
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-08-05 16:43:25 +0200

    add ao_crm, mail_activity_progress, mail_activity_purpose

---
+++ mail_activity_progress/models/mail_activity.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent <http://www.eficent.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+from openerp import api, fields, models
+
+
+class MailActivity(models.Model):
+
+    _inherit = 'mail.activity'
+
+    progress_id = fields.Many2one(
+        'mail.activity.progress', 'Progress',
+        index=True, ondelete='restrict')
+
+    @api.onchange('activity_type_id')
+    def _onchange_activity_type_id(self):
+        super(MailActivity, self)._onchange_activity_type_id()
+        if self.activity_type_id:
+            return {
+                'domain': {
+                    'progress_id': ['|',
+                                    ('activity_type_ids', 'in',
+                                     [self.activity_type_id.id]),
+                                    ('activity_type_ids', '=', False)]
+                },
+            }
+        return {'domain': {'progress_id': [
+            ('activity_type_ids', '=', False)]}}
