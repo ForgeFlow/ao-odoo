b4d8589dd277bb415a3b9705e96d2f33641c185d
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-08-05 16:43:25 +0200

    [9.0] add ao_crm, mail_activity_progress, mail_activity_purpose

--- ao_crm/models/crm.py
+++ ao_crm/models/crm.py
@@ -2,9 +2,13 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import fields, models
 
 
 class CrmLeads(models.Model):
     _inherit = 'crm.lead'
     _order = 'id DESC'
+
+    understanding_of_need = fields.Text('Understanding of Need')
+    understanding_of_impact = fields.Text('Understanding of Impact')
+    buying_process = fields.Text('Buying Process')
