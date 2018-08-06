52e6adca08bcdeef28c29ced7cd712416f1f65bf
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-08-06 17:50:14 +0200

    add migration of crm_activities to mail_activities

--- ao_crm/__openerp__.py
+++ ao_crm/__openerp__.py
@@ -4,11 +4,11 @@
 
 {
     "name": "AO-specific customizations on CRM",
-    "version": "9.0.1.0.0",
+    "version": "9.0.2.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "CRM",
-    "depends": ["crm"],
+    "depends": ["crm", "mail_activity_crm"],
     "data": [
         'views/crm_lead_views.xml',
     ],
