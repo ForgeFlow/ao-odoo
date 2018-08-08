ea64572f42f8331326cd60799ce50b1aefe3251b
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-08-08 13:52:08 +0200

    fixes on mail_activity_progress and mail_activity_purpose

--- mail_activity_purpose/models/mail_activity.py
+++ mail_activity_purpose/models/mail_activity.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Eficent <http://www.eficent.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-from openerp import api, fields, models
+from openerp import api, fields, models, _
+from openerp.exceptions import ValidationError
 
 
 class MailActivity(models.Model):
@@ -14,15 +15,27 @@ class MailActivity(models.Model):
 
     @api.onchange('activity_type_id')
     def _onchange_activity_type_id(self):
-        super(MailActivity, self)._onchange_activity_type_id()
+        res = super(MailActivity, self)._onchange_activity_type_id()
+        if self.activity_type_id not in self.purpose_id.activity_type_ids:
+            self.purpose_id = False
+        if not res:
+            res = {}
+        if 'domain' not in res:
+            res['domain'] = {}
         if self.activity_type_id:
-            return {
-                'domain': {
-                    'purpose_id': ['|',
-                                    ('activity_type_ids', 'in',
-                                     [self.activity_type_id.id]),
-                                    ('activity_type_ids', '=', False)]
-                },
-            }
-        return {'domain': {'purpose_id': [
-            ('activity_type_ids', '=', False)]}}
+            res['domain']['purpose_id'] = [
+                '|', ('activity_type_ids', 'in', [self.activity_type_id.id]),
+                ('activity_type_ids', 'in', [])]
+            return res
+        res['domain']['purpose_id'] = [('activity_type_ids', '=', False)]
+        return res
+
+    @api.constrains
+    def _constrain_purpose_activity_type(self):
+        for rec in self:
+            if rec.purpose_id and rec.activity_type_id not in \
+                    rec.purpose_id.activity_type_ids:
+                raise ValidationError(
+                    _('The purpose %s is not allowed for the '
+                      'selected activity type %s.') %
+                    (rec.purpose_id.name, rec.activity_type_id.name))
