b065e63444a0070a144ebf1f4a5a79b32e8b2eee
mreficent <miquel.raich@eficent.com>
2018-09-07 18:54:06 +0200

    [RENAME] ao_account_check_writing -> ao_account_check_printing

---
+++ ao_account_check_printing/README.rst
@@ -0,0 +1,21 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+=================
+AO Check Printing
+=================
+
+Customizes AO check form
+
+Features Added:
+---------------
+
+* Add the check number in the tree view of supplier payments. (*Invoicing >
+  Purchases > Payments*).
+
+Maintainers
+-----------
+
+* Jordi Ballester <jordi.ballester@eficent.com>
+* Lois Rilo <lois.rilo@eficent.com>
---
+++ ao_account_check_printing/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "AO Check Printing",
+    "version": "11.0.1.0.0",
+    "author": "Eficent",
+    "license": "AGPL-3",
+    "category": "Customizations",
+    "summary": "Customizes AO check form",
+    "website": "http://www.eficent.com",
+    "depends": ["account_check_printing"],
+    "data": [
+        "views/account_payment_view.xml",
+    ],
+    "installable": True,
+}
