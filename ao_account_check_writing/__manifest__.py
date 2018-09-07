6986b7e22f15ff626b0024f010eaeaa549c434d4
mreficent <miquel.raich@eficent.com>
2018-09-07 18:52:25 +0200

    [MIG] ao_account_check_writing: Migration to 11.0

--- ao_account_check_writing/__manifest__.py
+++ ao_account_check_writing/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "AO Check Writing",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent",
     "license": "AGPL-3",
-    "category": "Customizatins",
+    "category": "Customizations",
     "summary": "Customizes AO check form",
-    "website": "http://www.openerp.com",
+    "website": "http://www.eficent.com",
     "depends": ["account_check_printing"],
     "data": [
         "views/account_payment_view.xml",
