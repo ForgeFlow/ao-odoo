36d2c7df91efbb2cf612e4607b9258a90c06549c
mreficent <miquel.raich@eficent.com>
2018-09-14 14:33:39 +0200

    [MIG] ao_crm: Migration to 11.0

--- ao_crm/__manifest__.py
+++ ao_crm/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "AO-specific customizations on CRM",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "CRM",
-    "depends": ["crm", "mail_activity_crm"],
+    "depends": ["crm"],
     "data": [
         'views/crm_lead_views.xml',
     ],
