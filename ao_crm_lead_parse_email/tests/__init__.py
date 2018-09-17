1310bf9a4598b534e9ea74be87d144a0e6bd18ac
Lois Rilo <lois.rilo@eficent.com>
2018-09-17 16:35:01 +0200

    [11.0][MIG] ao_crm_lead_parse_email

---
+++ ao_crm_lead_parse_email/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_parse_email_lead
---
+++ ao_crm_lead_parse_email/tests/test_parse_email_lead.py
@@ -0,0 +1,38 @@
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests.common import TransactionCase
+from odoo.modules.module import get_module_resource
+
+
+class TestParseEmailLead(TransactionCase):
+    def setUp(self, *args, **kwargs):
+        super(TestParseEmailLead, self).setUp(*args, **kwargs)
+        self.partner_obj = self.env['res.partner']
+        self.lead_obj = self.env['crm.lead']
+
+        request_file = open(get_module_resource(
+            'ao_crm_lead_parse_email',
+            'tests', 'test_message.eml'), 'rb')
+        self.test_message = request_file.read()
+
+    def test_01_pase_email(self):
+        self.lead_obj.message_process('crm.lead', self.test_message)
+        lead = self.lead_obj.search(
+            [('email_from', '=', 'laura.f@customer.com'),
+             ('contact_name', '=', 'Laura Fincher')],
+            limit=1)
+        self.assertTrue(lead)
+        self.assertFalse(lead.partner_id)
+
+    def test_02_pase_email_existing_partner(self):
+        partner = self.partner_obj.create({
+            'name': 'Laura Fincher',
+            'email': 'laura.f@customer.com',
+        })
+        self.lead_obj.message_process('crm.lead', self.test_message)
+        lead = self.lead_obj.search(
+            [('email_from', '=', 'laura.f@customer.com'),
+             ('contact_name', '=', 'Laura Fincher')])
+        self.assertTrue(lead)
+        self.assertEqual(lead.partner_id, partner)
