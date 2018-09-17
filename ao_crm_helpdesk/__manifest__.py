0506131daf4f2acda5a3e98e7bdd70ff130b0d1f
mreficent <miquel.raich@eficent.com>
2018-09-17 15:43:21 +0200

    [MIG] ao_crm_helpdesk: Migration to 11.0

--- ao_crm_helpdesk/__manifest__.py
+++ ao_crm_helpdesk/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2015 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "AO-specific customizations on crm helpdesk",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "CRM",
