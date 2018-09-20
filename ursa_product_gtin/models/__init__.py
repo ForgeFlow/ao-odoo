81baa9e98a36a72ddfc2867bf71efe232c0ca90e
mreficent <miquel.raich@eficent.com>
2018-09-20 17:58:15 +0200

    [MIG] ursa_product_gtin: Migration to 11.0

--- ursa_product_gtin/models/__init__.py
+++ ursa_product_gtin/models/__init__.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2004 TINY SPRL. Fabien Pinckaers <fp@tiny.Be>
-# Copyright 2008 ChriCar Beteilugungs- und Beratungs
-#                Ferdinand Gassauer <tiny@chricar.at>
-# Copyright 2013 Ursa Information Systems (http://www.ursainfosystems.com).
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import ursa_product_gtin
