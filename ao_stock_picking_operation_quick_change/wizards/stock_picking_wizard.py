38d73ad882d78b04260ca2986247d2d0e6de4bcb
mreficent <miquel.raich@eficent.com>
2018-09-20 19:18:00 +0200

    [MIG] ao_stock_picking_operation_quick_change: Migration to 11.0

--- ao_stock_picking_operation_quick_change/wizards/stock_picking_wizard.py
+++ ao_stock_picking_operation_quick_change/wizards/stock_picking_wizard.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
 # © 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import _, api, fields, models
-from openerp.exceptions import UserError
+from odoo import _, api, fields, models
 
 
 class StockPickingOperationWizard(models.TransientModel):
