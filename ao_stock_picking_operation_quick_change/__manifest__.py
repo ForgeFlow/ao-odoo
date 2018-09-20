a749689ec3460d824fe4665cd55a91334da100f6
mreficent <miquel.raich@eficent.com>
2018-09-20 19:18:00 +0200

    [MIG] ao_stock_picking_operation_quick_change: Migration to 11.0

--- ao_stock_picking_operation_quick_change/__manifest__.py
+++ ao_stock_picking_operation_quick_change/__manifest__.py
@@ -1,16 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Lois Rilo <lois.rilo@eficent.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "AO customization for: Stock Picking Operation Quick Change",
     "summary": "Customization for Aleph Objects.",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Inventory",
     "website": "http://www.eficent.com",
     "author": "Eficent",
     "license": "AGPL-3",
-    "application": False,
     "installable": True,
     "depends": [
         "stock_picking_operation_quick_change",
