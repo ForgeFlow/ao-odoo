489f70098e547a3aee7ac92bdfcaac5dda05c861
Lois Rilo <lois.rilo@eficent.com>
2018-09-20 16:44:10 +0200

    [11.0][MIG] ao_crm_prospecting_status

--- ao_crm_prospecting_status/models/__init__.py
+++ ao_crm_prospecting_status/models/__init__.py
@@ -1,5 +1,2 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import crm_prospecting_status
 from . import res_partner
