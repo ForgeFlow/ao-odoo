6ca7a927f68be7734cef7a14d3f33a09a6cd1662
Lois Rilo <lois.rilo@eficent.com>
2018-09-20 11:08:18 +0200

    [11.0][MIG] stock_picking_menu

--- stock_picking_menu/__manifest__.py
+++ stock_picking_menu/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Stock Picking Menu",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "Stock",
