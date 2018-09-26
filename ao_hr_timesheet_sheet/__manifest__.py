9da6275eef514becc693984e72b617ed6f5d5042
mreficent <miquel.raich@eficent.com>
2018-09-26 20:53:06 +0200

    [MIG] ao_hr_timesheet_sheet: Migration to 11.0

--- ao_hr_timesheet_sheet/__manifest__.py
+++ ao_hr_timesheet_sheet/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-18 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "AO-specific customizations on HR Timesheet Sheet",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "Human Resources",
