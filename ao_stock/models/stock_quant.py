7f9be5d407afbb5cda28583088305a28a971c2c0
mreficent <miquel.raich@eficent.com>
2018-09-27 14:50:04 +0200

    [MIG] ao_stock: Migration to 11.0

--- ao_stock/models/stock_quant.py
+++ ao_stock/models/stock_quant.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
-class StockQuant(models.Model):
+class Quant(models.Model):
     _inherit = "stock.quant"
 
     # TODO: to remove?
