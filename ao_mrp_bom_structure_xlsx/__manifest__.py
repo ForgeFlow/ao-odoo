fea18737f18776c7e72eda8a936ad80b40bc35f8
mreficent <miquel.raich@eficent.com>
2018-09-28 13:58:43 +0200

    [MIG] ao_mrp_bom_structure_xlsx: Migration to 11.0

--- ao_mrp_bom_structure_xlsx/__manifest__.py
+++ ao_mrp_bom_structure_xlsx/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': "AO - Export BoM Structure to Excel",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Manufacturing',
     'author': "Eficent",
     'license': 'AGPL-3',
