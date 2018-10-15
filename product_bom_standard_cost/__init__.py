948e4d946c3d6d0ca2fe801e469a6101bbfbfe44
aheficent <ahenriquez@eficent.com>
2018-10-15 11:38:18 +0200

    [REV]bom_standard_cost should not be stored

--- product_bom_standard_cost/__init__.py
+++ product_bom_standard_cost/__init__.py
@@ -2,4 +2,3 @@
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import models
-from .hooks import pre_init_hook
