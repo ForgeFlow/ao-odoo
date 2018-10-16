80e8382906572ae485c5a576e0533bbef86f28a6
Lois Rilo <lois.rilo@eficent.com>
2018-10-16 17:21:08 +0200

    [9.0][FIX] mail_activity_progress: propagate to calendar events.

--- mail_activity_progress/__openerp__.py
+++ mail_activity_progress/__openerp__.py
@@ -16,5 +16,6 @@
         'security/ir.model.access.csv',
         'views/mail_activity_progress_views.xml',
         'views/mail_activity_views.xml',
+        'views/calendar_event_view.xml',
     ],
 }
