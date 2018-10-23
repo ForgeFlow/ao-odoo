0e0eac998006928947e0b9f13dcbc28a97a6c3cd
Lois Rilo <lois.rilo@eficent.com>
2018-10-23 17:17:55 +0200

    [11.0][MIG] mail_activity_purpose

--- mail_activity_purpose/models/calendar_event.py
+++ mail_activity_purpose/models/calendar_event.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Odoo SA <https://www.odoo.com>
 # Copyright 2018 Eficent <http://www.eficent.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-from openerp import api, models, fields
+
+from odoo import fields, models
 
 
 class CalendarEvent(models.Model):
--- mail_activity_purpose/models/mail_activity_purpose.py
+++ mail_activity_purpose/models/mail_activity_purpose.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent <http://www.eficent.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-from openerp import fields, models
+from odoo import fields, models
 
 
 class MailActivityPurpose(models.Model):
