596e7aa1aedf2bca9b67d777c20c52786ce269eb
Lois Rilo <lois.rilo@eficent.com>
2018-10-23 13:06:46 +0200

    [11.0][MIG] crm_helpdesk_add_suggested_recipient

--- crm_helpdesk_add_suggested_recipient/__manifest__.py
+++ crm_helpdesk_add_suggested_recipient/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # © 2015 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "CRM Helpdesk Add Suggested Recipient",
     "summary": "Auto suggest the partner in chatter mail responses",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.,"
               "Odoo Community Association (OCA)",
     "website": "https://www.eficent.com",
