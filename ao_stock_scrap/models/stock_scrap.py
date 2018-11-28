2dde5e01c1e4ed0ffa2b52946e83068a7c8cf818
mreficent <miquel.raich@eficent.com>
2018-11-28 17:02:34 +0100

    [MIG] ao_stock_scrap: Migration to 11.0

--- ao_stock_scrap/models/stock_scrap.py
+++ ao_stock_scrap/models/stock_scrap.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -12,4 +11,3 @@ class StockScrap(models.Model):
         comodel_name='res.company', string='Company', required=True,
         default=lambda self: self.env.user.company_id,
     )
-    scrap_location_id = fields.Many2one(required=True)  # v11 MIG: remote this.
