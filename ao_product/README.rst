6e16e6e8f173c7a60cb86c357981898620a7883b
Lois Rilo <lois.rilo@eficent.com>
2018-12-12 14:30:04 -0700

    [11.0][ADD] ao_product

---
+++ ao_product/README.rst
@@ -0,0 +1,19 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+=======================================
+AO-specific customizations for Products
+=======================================
+
+This module contains customizations specific to Aleph Objects.
+
+* Add button in the button box to stock moves.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Lois Rilo <lois.rilo@eficent.com>
---
+++ ao_product/models/product.py
@@ -0,0 +1,26 @@
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.multi
+    def action_view_stock_moves(self):
+        self.ensure_one()
+        action = self.env.ref('stock.stock_move_action').read()[0]
+        action['domain'] = [('product_id', '=', self.id)]
+        return action
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.multi
+    def action_view_stock_moves(self):
+        self.ensure_one()
+        action = self.env.ref('stock.stock_move_action').read()[0]
+        action['domain'] = [('product_id.product_tmpl_id', 'in', self.ids)]
+        return action
