654148e9134c68b1ca242693c3bbdbcf406f1d93
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-01-09 11:31:02 +0100

    fixes OCA Trouble Tickets - Task #20587

---
+++ ao_account/models/account_invoice.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 Aleph Objects, Inc.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.model
+    def create(self, vals):
+        return super(
+            AccountInvoice, self.with_context(
+                mail_auto_subscribe_no_notify=True)).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        return super(
+            AccountInvoice, self.with_context(
+                mail_auto_subscribe_no_notify=True)).write(vals)
