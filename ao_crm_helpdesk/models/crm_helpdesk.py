fca092772826dd964c972b1ea707c46c8dc1b84c
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-01-14 15:37:27 +0100

    fixup

--- ao_crm_helpdesk/models/crm_helpdesk.py
+++ ao_crm_helpdesk/models/crm_helpdesk.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
