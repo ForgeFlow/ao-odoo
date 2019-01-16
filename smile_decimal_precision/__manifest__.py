4b24379a92650da4ef927fbde59581fa92aa3d35
Lois Rilo <lois.rilo@eficent.com>
2019-01-16 15:29:47 +0100

    [11.0][ADD] smile_decimal_precision (#97)

---
+++ smile_decimal_precision/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# (C) 2012 Smile (<http://www.smile.fr>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Display Decimal Precision",
+    "version": "11.0.1.0.0",
+    "depends": ["decimal_precision"],
+    "author": "Smile",
+    "license": 'AGPL-3',
+    "description": """
+This module allows to distinguish computation digits
+and display digits in decimal precision.
+""",
+    "website": "http://www.smile.fr",
+    "category": "Hidden/Dependency",
+    "sequence": 32,
+    "data": [
+        "views/decimal_precision_view.xml",
+        "views/res_currency_view.xml",
+    ],
+    "auto_install": True,
+    "installable": True,
+    "application": False,
+}
