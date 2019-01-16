a31e7a7f277e7f0c6ca1ecddeac01ce9d6f859fe
Lois Rilo <lois.rilo@eficent.com>
2019-01-16 17:06:43 +0100

    [11.0][FIX] smile_decimal_precision: avoid keyerror

--- smile_decimal_precision/report/ir_qweb.py
+++ smile_decimal_precision/report/ir_qweb.py
@@ -8,8 +8,8 @@ from odoo.addons.base.ir.ir_qweb.fields import FloatConverter
 def record_to_html(self, record, field_name, options):
     if 'precision' not in options and 'decimal_precision' not in options:
         _, precision = \
-            record._fields[field_name].get_description(self.env)['digits'] or \
-            (None, None)
+            record._fields[field_name].get_description(
+                self.env).get('digits') or (None, None)
         options = dict(options, precision=precision)
     return super(FloatConverter, self).record_to_html(
         record, field_name, options)
