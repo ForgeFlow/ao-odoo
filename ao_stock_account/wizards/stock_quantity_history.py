164a61c72e97f33ada6ecb4d9d05c2a0ea38c8cb
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-01-17 14:49:52 +0100

    [IMP] Datetime to Date field in stock valuation wizard

--- ao_stock_account/wizards/stock_quantity_history.py
+++ ao_stock_account/wizards/stock_quantity_history.py
@@ -3,14 +3,23 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models
+from datetime import datetime
 
 
 class StockQuantityHistory(models.TransientModel):
     _inherit = 'stock.quantity.history'
 
     def open_table(self):
+        self.ensure_one()
+        if self.compute_at_date:
+            self.date = datetime.strptime(
+                "%s %s" % (self.date_wizard, "23:59:59.9999"),
+                '%Y-%m-%d %H:%M:%S.%f')
         action = super(StockQuantityHistory, self).open_table()
         if self.compute_at_date:
+            action.update({
+                'context': dict(self.env.context, to_date=self.date)
+            })
             tree_view_id = self.env.ref(
                 'ao_stock_account.view_stock_product_tree2').id
             action['views'][0] = (tree_view_id, 'tree')
