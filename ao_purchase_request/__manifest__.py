318bd345c9e45853258571185999d70ff0a42783
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-02-04 13:16:53 +0100

    [ao_purchase_request][add]

---
+++ ao_purchase_request/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 Aleph Objects, Inc.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "AO-specific customizations on purchase request",
+    "version": "11.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "website": "http://www.eficent.com",
+    "category": "Product",
+    "depends": ["purchase_request"],
+    "license": "AGPL-3",
+    'installable': True,
+}
---
+++ ao_purchase_request/models/procurement_rule.py
@@ -0,0 +1,14 @@
+# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class ProcurementRule(models.Model):
+    _inherit = 'procurement.rule'
+
+    @api.model
+    def _make_pr_get_domain(self, values):
+        super(ProcurementRule, self)._make_pr_get_domain(values)
+        domain = (('id', '=', 0),)
+        return domain
