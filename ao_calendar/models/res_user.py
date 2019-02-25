41a2ed8bc8fe7e82922d7cd57f48f1d19670a821
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-02-25 19:59:55 +0100

    Fix user interface

--- ao_calendar/models/res_user.py
+++ ao_calendar/models/res_user.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import models, fields
 
@@ -7,6 +7,8 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     calendar_allow_ui_edition = fields.Boolean(
-        string="Allow UI calendar edition",
+        string="Allow in-calendar edition",
+        help="When set you can modify the dates of existing calendar events "
+             "by moving them in the calendar view.",
         default=True,
     )
