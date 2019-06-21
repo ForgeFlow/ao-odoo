e535154565a08538596e78b9e0e975684624319f
Lois Rilo <lois.rilo@eficent.com>
2019-06-21 13:41:20 +0200

    [11.0][ADD] ao_purchase_auto_close

---
+++ ao_purchase_auto_close/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# - Lois Rilo Antelo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "AO Purchase Auto Close",
+    "version": "11.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "website": "http://www.eficent.com",
+    "category": "Purchases",
+    "depends": [
+        "purchase",
+        "purchase_open_qty",
+    ],
+    "data": [
+        "data/ir_cron.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
