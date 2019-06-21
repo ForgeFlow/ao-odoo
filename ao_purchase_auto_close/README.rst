697c3f4abdcd09d179abeeb4a03fa3c89b9c4df8
Lois Rilo <lois.rilo@eficent.com>
2019-06-21 13:41:20 +0200

    [11.0][ADD] ao_purchase_auto_close

---
+++ ao_purchase_auto_close/README.rst
@@ -0,0 +1,22 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+======================
+AO Purchase Auto Close
+======================
+
+This module contains customizations specific to Aleph Objects. Adds an
+scheduled action that locks Purchase Orders that are fully invoiced and
+received.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Eficent Business and IT Consulting Services S.L. <contact@eficent.com>
+
+  - Lois Rilo <lois.rilo@eficent.com>
---
+++ ao_purchase_auto_close/models/purchase_order.py
@@ -0,0 +1,33 @@
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    @api.model
+    def _get_domain_auto_close_po(self):
+        return [
+            ("state", "=", "purchase"),
+            ("invoice_status", "=", "invoiced"),
+            ("qty_to_invoice", "=", 0),
+            ("qty_to_receive", "=", 0),
+        ]
+
+    @api.model
+    def cron_ao_auto_close_purchases(self):
+        """Auto-close PO's.
+        """
+        _logger.info("Start job")
+        domain = self._get_domain_auto_close_po()
+        completed_pos = self.search(domain)
+        completed_pos.write({
+            "state": "done",
+        })
+        _logger.info("Closed %s Purchase Orders" % len(completed_pos))
+        return True
