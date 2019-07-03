67b4a1daecf636e51f188277b682b04e99d3aab7
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-07-03 16:04:46 +0200

    [ADD] force default Conversion Action value

---
+++ ao_crm/wizard/crm_lead_to_opportunity.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class Lead2OpportunityPartner(models.TransientModel):
+    _inherit = 'crm.lead2opportunity.partner'
+
+    @api.model
+    def default_get(self, fields):
+        result = super(Lead2OpportunityPartner, self).default_get(fields)
+        # Force Convert Action to be Convert to opportunity as default always
+        result['name'] = 'convert'
+        return result
