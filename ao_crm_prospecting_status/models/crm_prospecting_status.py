72db2132b5832f1f336f6805308a364b4eec030d
Lois Rilo <lois.rilo@eficent.com>
2019-07-05 09:46:11 +0200

    [12.0][MIG] ao_crm_prospecting_status

--- ao_crm_prospecting_status/models/crm_prospecting_status.py
+++ ao_crm_prospecting_status/models/crm_prospecting_status.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018-19 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -7,8 +7,9 @@ from odoo import fields, models
 class CrmProspectingStatus(models.Model):
     _name = 'crm.prospecting.status'
     _order = 'sequence'
+    _description = "Prospecting Status"
 
-    name = fields.Char('Name', required=True)
-    sequence = fields.Integer('Sequence', default=10)
-    summary = fields.Text('Summary')
+    name = fields.Char(required=True)
+    sequence = fields.Integer(default=10)
+    summary = fields.Text()
     active = fields.Boolean(default=True)
