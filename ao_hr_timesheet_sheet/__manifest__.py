ddc1bcf6b37c640e7772f73edf97e0f949ef196f
Lois Rilo <lois.rilo@eficent.com>
2019-07-05 12:30:18 +0200

    [12.0][MIG] ao_hr_timesheet_sheet

--- ao_hr_timesheet_sheet/__manifest__.py
+++ ao_hr_timesheet_sheet/__manifest__.py
@@ -1,15 +1,17 @@
-# Copyright 2015-18 Eficent Business and IT Consulting Services S.L.
+# Copyright 2015-19 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "AO-specific customizations on HR Timesheet Sheet",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.",
     "website": "http://www.eficent.com",
     "category": "Human Resources",
-    "depends": ["hr_timesheet_sheet",
-                "hr_timesheet_attendance"],
+    "depends": [
+        "hr_timesheet_sheet",
+        "hr_timesheet_attendance",
+    ],
     "license": "AGPL-3",
     "data": [
         "security/hr_timesheet_sheet_security.xml",
