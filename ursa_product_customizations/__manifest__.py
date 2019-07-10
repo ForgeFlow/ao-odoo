093a1017917c699d894329f86a407841002f617c
Lois Rilo <lois.rilo@eficent.com>
2019-07-10 16:07:36 +0200

    [12.0][MIG] ursa_product_customizations

--- ursa_product_customizations/__manifest__.py
+++ ursa_product_customizations/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Product Customizations",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Ursa Information Systems, USA",
     "category": "Product",
