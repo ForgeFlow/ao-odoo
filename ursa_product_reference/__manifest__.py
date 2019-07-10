9de9a18daed476c03286b81b64eee9fc204eac9b
Lois Rilo <lois.rilo@eficent.com>
2019-07-10 16:21:51 +0200

    [12.0][MIG] ursa_product_reference

--- ursa_product_reference/__manifest__.py
+++ ursa_product_reference/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Product Internal Reference Generation",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Ursa Information Systems, USA",
     "license": "AGPL-3",
     "category": "Stock",
