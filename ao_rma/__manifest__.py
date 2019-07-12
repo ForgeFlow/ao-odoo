88e75ce306049e7c70176e6c84ca68607efa6adb
Aaron Henriquez <ahenriquez@eficent.com>
2019-07-12 15:48:44 +0200

    [ADD]ao_rma customizations

---
+++ ao_rma/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2015-18 Eficent Business and IT Consulting Services S.L.
+# - Héctor Villarreal Ortega
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "RMA Customizations",
+    "version": "11.0.1.0.0",
+    "author": "Eficent",
+    "website": "http://www.eficent.com",
+    "category": "rma",
+    "depends": ["rma",
+                ],
+    "data": ["views/rma_operation_view.xml",
+             "views/rma_order_line_view.xml"],
+    "license": "AGPL-3",
+    'installable': True,
+}
