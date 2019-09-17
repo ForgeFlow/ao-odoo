78c338f72b3e49bb557343ea537198ebe9764fdb
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-09-17 11:23:29 +0200

    [12.0][ADD] ao_base shipping_address_type

--- ao_base/models/res_partner.py
+++ ao_base/models/res_partner.py
@@ -1,11 +1,19 @@
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
-class ResUsers(models.Model):
+class ResPartner(models.Model):
     _inherit = "res.partner"
+
+    shipping_address_type = fields.Selection(
+        [('residential', 'Residential'),
+         ('commercial', 'Commercial'),
+         ('educational', 'Educational'),
+         ('government', 'Government')],
+        string='Shipping Address Type'
+    )
 
     @api.onchange('state_id')
     def onchange_state(self):
